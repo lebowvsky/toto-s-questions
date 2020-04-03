07d657ce84f0a52d5dee7c1d7a3d0e1e1b5886f1
Brice Le Gallo <lebowvsky@gmail.com>
2020-04-03 19:10:58 +0200

    pour faire joli

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const dictionnary = ["Dev", "l'UX", "WordPress", "Submit", "Responsive", "React", "Redux", "Bootstrap", "JQuery", "Les props", "Le CSS"]
+const dictionnary = ["Dev", "l'UX", "WordPress", "Submit", "Responsive", "React", "Redux", "Bootstrap", "JQuery", "Les props", "Le CSS", "WebAssembly"]
+
+const questionPlace = document.getElementById('question');
+const button = document.getElementById('btn');
+
+function getRandomDictionnary(arr){
+  questionPlace.innerHTML = ""
+  const i = Math.floor(Math.random() * Math.floor(arr.length));
+  const j = Math.floor(Math.random() * Math.floor(arr.length));
+  console.log(`Est-ce que ${dictionnary[i]} c'est du ${dictionnary[j]} ?`);
+  questionPlace.innerHTML += `<p>"Est-ce que ${dictionnary[i]} c'est du ${dictionnary[j]} ?"</p>`
+}
+
+getRandomDictionnary(dictionnary);
+
+button.addEventListener('click', () => {
+  getRandomDictionnary(dictionnary);
+})
